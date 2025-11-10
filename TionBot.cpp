984c3e640a75d7646011f0f8c0ee75ad4d2f3cc5
Harini B R <harinisonubr@gmail.com>
2025-11-10 23:40:42 +0530

    Update WiFi credentials in TionBot.cpp

--- TionBot.cpp
+++ TionBot.cpp
@@ -3,8 +3,8 @@
 //=========================
 // 🔹 WiFi credentials
 //=========================a
-const char* ssid = "Rajashree";
-const char* password = "9940356774";
+const char* ssid = "xxx";
+const char* password = "yyy";
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 String voiceCommand = "";
